9f643829577a94cb78a29800199bccc0e9b592fa
Alex Costa <alexcostaluiz@outlook.com>
2017-10-15 18:03:33 -0400

    complete employment layout

--- app/src/main/java/com/example/alex/interactiveresume/JobActivity.java
+++ app/src/main/java/com/example/alex/interactiveresume/JobActivity.java
@@ -3,21 +3,18 @@ package com.example.alex.interactiveresume;
 import android.animation.Animator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.transition.Transition;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 
 public class JobActivity extends AppCompatActivity {
 
     RelativeLayout toolbar;
-    LinearLayout jobContainer;
+    ScrollView scroller;
     ImageView back;
-    boolean activityLoaded = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +22,9 @@ public class JobActivity extends AppCompatActivity {
         setContentView(R.layout.job_layout);
 
         toolbar = (RelativeLayout) findViewById(R.id.job_toolbar);
-        jobContainer = (LinearLayout) findViewById(R.id.job_container);
         back = (ImageView) findViewById(R.id.job_back);
-        jobContainer.setY(MainActivity.SCREEN_HEIGHT-50);
+        scroller = (ScrollView) findViewById(R.id.job_scrollview);
+        scroller.setY(MainActivity.SCREEN_HEIGHT-50);
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,33 +51,12 @@ public class JobActivity extends AppCompatActivity {
             }
         });
 
-        jobContainer.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+        scroller.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
             @Override
             public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
                 v.removeOnLayoutChangeListener(this);
                 float y = toolbar.getY() + toolbar.getHeight() + getResources().getDimension(R.dimen.margin);
-                jobContainer.animate().y(y).setDuration(300).setStartDelay(100).setInterpolator(new DecelerateInterpolator())
-                        .setListener(new Animator.AnimatorListener() {
-                            @Override
-                            public void onAnimationStart(Animator animation) {
-
-                            }
-
-                            @Override
-                            public void onAnimationEnd(Animator animation) {
-                                activityLoaded = true;
-                            }
-
-                            @Override
-                            public void onAnimationCancel(Animator animation) {
-
-                            }
-
-                            @Override
-                            public void onAnimationRepeat(Animator animation) {
-
-                            }
-                        }).start();
+                scroller.animate().y(y).setDuration(300).setStartDelay(100).setInterpolator(new DecelerateInterpolator()).start();
             }
         });
     }
